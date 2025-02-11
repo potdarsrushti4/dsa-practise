7ef1b76cadf69b9e32792d7275ba66a9bfb75f73
srushti <potdarsrushti4@gmail.com>
2025-02-11 20:27:22 +0530

    2D arrays

---
+++ Matrices.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Matrices{
+    public static void main(String[] args)
+    {
+        int[][] arr=new int[3][3];
+        Scanner sc=new Scanner(System.in);
+
+        for(int i=0;i<3;i++)
+        {
+            System.out.print("Enter "+i+"th row : ");
+            for(int j=0;j<3;j++)
+            {
+                
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0;i<3;i++){
+            for (int j=0;j<3;j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println("");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
+}
+
+
