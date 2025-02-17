504139dffb7f44162dd59c6381ea4f4225f0c8b9
srushti <potdarsrushti4@gmail.com>
2025-02-17 23:59:25 +0530

    printing subarrays

---
+++ subarrays.java
@@ -0,0 +1,10 @@
+public class subarrays{
+
+    public static void printsubarrays(int[] arr){
+        
+    }
+    public static void main(String[] args) {
+        int[] arr={2,4,6,8,10};
+        printsubarrays(arr);
+    }
+}
