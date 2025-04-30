0d52d2c1b55339803896f189cecbd4abe906c598
srushti <potdarsrushti4@gmail.com>
2025-04-30 20:47:59 +0530

    star patterns

---
+++ Inverted_star.java
@@ -0,0 +1,13 @@
+public class Inverted_star{
+    public static void main(String[] args)
+    {
+        for(int i = 8 ; i > 0 ; i--)
+        {
+            for(int j = i ; j > 0 ; j--)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
