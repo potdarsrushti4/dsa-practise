8b2eb153a07c98c7b9491a37fb89f62ac43febdd
srushti <potdarsrushti4@gmail.com>
2025-06-02 01:57:58 +0530

    Binary strings prblm using recursion

---
+++ Binary_strings.java
@@ -0,0 +1,34 @@
+public class Binary_strings{
+
+        public static void binaryStrings(int n,int i,String str)
+        {
+            //base case
+            if(n==0)
+            {
+                System.out.println(str);
+                return;
+            }
+            //actual work
+            //i mtlb last positon pe konsa char hai
+            if(i==0)
+            {
+                binaryStrings(n-1, 0, str+"0"); // 2 1 0 4 
+                binaryStrings(n-1, 1, str+"1"); // 0 1 2 
+            }
+            else{
+                binaryStrings(n-1, 0, str+"0"); // 1 
+            }
+            // binaryStrings(n-1, 0, str+"0");
+
+            // if(i==0)
+            // {
+            //     binaryStrings(n-1, 1, str+"1");
+            // }
+
+            // System.out.println(str);
+        }
+        public static void main(String[] args)
+        {
+            binaryStrings(3, 0, " ");
+        }
+}
