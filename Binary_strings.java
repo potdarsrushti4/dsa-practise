0e3c2e9f7dd69a0a7ba02b739df776786df0a129
srushti <potdarsrushti4@gmail.com>
2025-07-09 18:08:29 +0530

    Binary Strings

--- Binary_strings.java
+++ Binary_strings.java
@@ -12,11 +12,11 @@ public class Binary_strings{
             //i mtlb last positon pe konsa char hai
             if(i==0)
             {
-                binaryStrings(n-1, 0, str+"0"); // 2 1 0 4 
-                binaryStrings(n-1, 1, str+"1"); // 0 1 2 
+                binaryStrings(n-1, 0, str+"0"); // 1 2 3 4
+                binaryStrings(n-1, 1, str+"1"); // 1 2 3 
             }
             else{
-                binaryStrings(n-1, 0, str+"0"); // 1 
+                binaryStrings(n-1, 0, str+"0"); // 1 2 
             }
             // binaryStrings(n-1, 0, str+"0");
 
