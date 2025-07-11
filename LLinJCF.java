d11446ae5feead9cbc1875894c9d5cb9bd0e7ae3
srushti <potdarsrushti4@gmail.com>
2025-07-12 02:28:20 +0530

    LL using JCF

---
+++ LLinJCF.java
@@ -0,0 +1,13 @@
+//Linked in Collections Framework
+import java.util.LinkedList;
+
+public class LLinJCF{
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        System.out.println(ll);
+    }
+}
