77e844146a21a7cbf0626f3f769864df396a7504
srushti <potdarsrushti4@gmail.com>
2025-07-25 11:43:48 +0530

    Level order in binary tree

---
+++ LevelOrder.java
@@ -0,0 +1,24 @@
+//binary tree
+//level order traversal
+public class LevelOrder{
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class BinaryTree{
+        public Node buildTree(){
+            
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
